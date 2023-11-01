314e51f699d79b7ac39fc4f6d9489e32e5fff897
Sumanth Poojary <128285327+sumanthpoojary-afk@users.noreply.github.com>
2023-11-02 00:27:06 +0530

    Update script.js
    Refactored the  javascript code

--- Guess the Number/script.js
+++ Guess the Number/script.js
@@ -4,70 +4,63 @@ let secnumber = Math.floor(Math.random() * 20 + 1);
 let score = 20;
 let highScore = 0;
 
+const displayMessage = function (message) {
+  document.querySelector('.message').textContent = message;
+};
+const displayNumber = function (number) {
+  document.querySelector('.number').textContent = number;
+};
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   // when there is not input
   if (!guess) {
-    document.querySelector('.message').textContent = '🚫 Enter a Number!';
+    displayMessage('🚫 Enter a Number!');
   }
   // when guess is correct
   else if (guess === secnumber) {
-    document.querySelector('.message').textContent = '🎉 Correct Number!!';
-    document.querySelector('.number').textContent = guess;
+    displayMessage('🎉 Correct Number!!');
+    displayNumber(guess);
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.btn').style.backgroundColor = '#a0d191';
     document.querySelector('.guess').style.backgroundColor = '#a0d191';
     document.querySelector('.check').style.backgroundColor = '#a0d191';
     document.querySelector('.number').style.width = '30rem';
 
-    document.querySelector('.number').textContent = secnumber;
+    displayNumber(secnumber);
     confetti();
     if (score > highScore) {
       highScore = score;
       document.querySelector('.highscore').textContent = highScore;
     }
   }
-  // when guess is gretater
-  else if (guess > secnumber) {
+  // when Guess is wrong
+  else if (guess !== secnumber) {
     if (score > 1) {
-      document.querySelector('.message').textContent = '📈 Too High';
+      displayMessage(guess > secnumber ? '📈 Too High' : '📉 Too low');
       score--;
       document.querySelector('.score').textContent = score;
     } else {
-      document.querySelector('.message').textContent = '🤯 You loss';
-      document.querySelector('.score').textContent = 0;
-    }
-  }
-  // When guess is low
-  else if (guess < secnumber) {
-    if (score > 1) {
-      document.querySelector('.message').textContent = '📉 Too low';
-      score--;
-      document.querySelector('.score').textContent = score;
-    } else {
-      document.querySelector('.message').textContent = '🤯 You loss';
+      displayMessage('🤯 You lost the Game');
       document.querySelector('.score').textContent = 0;
     }
   }
 
   // when guess is greater than 20
   if (guess > 20) {
-    document.querySelector('.message').textContent =
-      '🚫 Enter a Smaller Number!';
+    displayMessage('🚫 Enter a Smaller Number!)');
   }
 
   // when guess is less than 0
   if (guess < 0) {
-    document.querySelector('.message').textContent =
-      '🚫 Enter a Larger Number!';
+    displayMessage('🚫 Enter a Larger Number!');
   }
 });
 
 document.querySelector('.again').addEventListener('click', function () {
   let secnumber = Math.floor(Math.random() * 20 + 1);
-  document.querySelector('.number').textContent = secnumber;
+  displayNumber(secnumber);
   let score = 20;
-  document.querySelector('.message').textContent = 'Start Guessing...';
+  displayMessage('Start Guessing...');
   document.querySelector('.score').textContent = score;
   document.querySelector('.number').textContent = '?';
 
