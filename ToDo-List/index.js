2fee8998bdeb850a2e35ed023cb1f66d2e80a18c
Sumanth Poojary <128285327+sumanthpoojary-afk@users.noreply.github.com>
2024-02-20 17:58:15 +0530

    Add files via upload

---
+++ ToDo-List/index.js
@@ -0,0 +1,23 @@
+"use strict";
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
+const addTask = function () {
+  if (inputBox.value === "") alert("Write your Task!");
+  else {
+    let li = document.createElement("li");
+    li.innerHTML = inputBox.value;
+    listContainer.appendChild(li);
+    ///
+    let span = document.createElement("span");
+    span.innerHTML = "\u00d7";
+    li.appendChild(span);
+  }
+  inputBox.value = "";
+};
+listContainer.addEventListener("click", function (e) {
+  if (e.target.tagName === "LI") {
+    e.target.classList.toggle("checked");
+  } else if (e.target.tagName === "SPAN") {
+    e.target.parentElement.remove();
+  }
+});
